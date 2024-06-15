880f54574a2bc1e3baedb78f7a0042d8f847d49d
cMerd <mardjafarzadeh@gmail.com>
2024-06-15 14:50:46 +0400

    Clean the code a little

--- Music Program.py
+++ Music Program.py
@@ -2,83 +2,90 @@ import tkinter as tk
 import winsound
 import threading
 
+# @WARNING: Constants, do not edit or reassign
+DEFAULT_FONT = "Verdana 14 bold"
+DEFAULT_TITLE = "Piyano"
+DEFAULT_WIDTH = 520
+DEFAULT_HEIGHT = 400
+DEFAULT_DIMENSIONS = str(DEFAULT_WIDTH) + 'x' + str(DEFAULT_HEIGHT)
+BG_COL = "white"
+FG_COL = "black"
+
+KEY_HEIGHT = 10
+KEY_WIDTH = 3
+
+
 def play_sound(filename):
+    # @param: filename - string; name of audio file to be played
+    # @return: no return
     def _play():
         winsound.PlaySound(filename, winsound.SND_FILENAME)
     threading.Thread(target=_play).start()
 
-def cal(event=None):
+
+def play_note(note):
+    # @param: note - string; name of note to be played (eg. 'do', 're', 'mi')
+    # @return: no return
     b1.config(relief=tk.SUNKEN)
-    play_sound('nota-do.wav')
+    play_sound("./notes/nota-" + note + ".wav")
     b1.after(30, lambda: b1.config(relief=tk.RAISED))
 
-def re(event=None):
-    b2.config(relief=tk.SUNKEN)
-    play_sound('nota-re.wav')
-    b2.after(30, lambda: b2.config(relief=tk.RAISED))
-
-def mi(event=None):
-    b3.config(relief=tk.SUNKEN)
-    play_sound('nota-mi.wav')
-    b3.after(30, lambda: b3.config(relief=tk.RAISED))
-
-def fa(event=None):
-    b4.config(relief=tk.SUNKEN)
-    play_sound('nota-fa.wav')
-    b4.after(30, lambda: b4.config(relief=tk.RAISED))
-
-def sol(event=None):
-    b5.config(relief=tk.SUNKEN)
-    play_sound('nota-sol.wav')
-    b5.after(30, lambda: b5.config(relief=tk.RAISED))
-
-def la(event=None):
-    b6.config(relief=tk.SUNKEN)
-    play_sound('nota-lya.wav')
-    b6.after(30, lambda: b6.config(relief=tk.RAISED))
-
-def si(event=None):
-    b7.config(relief=tk.SUNKEN)
-    play_sound('nota-si.wav')
-    b7.after(30, lambda: b7.config(relief=tk.RAISED))
 
 def otomatik():
+    # @TODO: Implement automation
     pass
 
-pencere = tk.Tk()
-pencere.title('Piyano')
-pencere.geometry('520x400')
 
-b1 = tk.Button(pencere, text='Do', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=cal)
-b1.place(x=50, y=20)
+if __name__ == "__main__":
+    pencere = tk.Tk()
+    pencere.title(DEFAULT_TITLE)
+    pencere.geometry(DEFAULT_DIMENSIONS)
 
-b2 = tk.Button(pencere, text='Re', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=re)
-b2.place(x=110, y=20)
+    b1 = tk.Button(pencere, text='Do', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('do'))
+    b1.place(x=50, y=20)
 
-b3 = tk.Button(pencere, text='Mi', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=mi)
-b3.place(x=170, y=20)
+    b2 = tk.Button(pencere, text='Re', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('re'))
+    b2.place(x=110, y=20)
 
-b4 = tk.Button(pencere, text='Fa', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=fa)
-b4.place(x=230, y=20)
+    b3 = tk.Button(pencere, text='Mi', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('mi'))
+    b3.place(x=170, y=20)
 
-b5 = tk.Button(pencere, text='Sol', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=sol)
-b5.place(x=290, y=20)
+    b4 = tk.Button(pencere, text='Fa', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('fa'))
+    b4.place(x=230, y=20)
 
-b6 = tk.Button(pencere, text='Lâ', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=la)
-b6.place(x=350, y=20)
+    b5 = tk.Button(pencere, text='Sol', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('sol'))
+    b5.place(x=290, y=20)
 
-b7 = tk.Button(pencere, text='Si', font='Verdana 14 bold', bg='white', fg='black', height=10, width=3, command=si)
-b7.place(x=410, y=20)
+    b6 = tk.Button(pencere, text='Lâ', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('lya'))
+    b6.place(x=350, y=20)
 
-b8 = tk.Button(pencere, text='Otomatik', font='Verdana 14 bold', bg='white', fg='black', height=1, width=10, command=otomatik)
-b8.place(x=175, y=300)
+    b7 = tk.Button(pencere, text='Si', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=KEY_HEIGHT, width=KEY_WIDTH,
+                   command=lambda: play_note('si'))
+    b7.place(x=410, y=20)
 
-pencere.bind('<a>', cal)
-pencere.bind('<s>', re)
-pencere.bind('<d>', mi)
-pencere.bind('<f>', fa)
-pencere.bind('<g>', sol)
-pencere.bind('<h>', la)
-pencere.bind('<j>', si)
+    b8 = tk.Button(pencere, text='Otomatik', font=DEFAULT_FONT,
+                   bg=BG_COL, fg=FG_COL, height=1, width=10, command=otomatik)
+    b8.place(x=175, y=300)
 
-pencere.mainloop()
+    pencere.bind('<a>', lambda: play_note('do'))
+    pencere.bind('<s>', lambda: play_note('re'))
+    pencere.bind('<d>', lambda: play_note('mi'))
+    pencere.bind('<f>', lambda: play_note('fa'))
+    pencere.bind('<g>', lambda: play_note('sol'))
+    pencere.bind('<h>', lambda: play_note('la'))
+    pencere.bind('<j>', lambda: play_note('si'))
+
+    pencere.mainloop()
